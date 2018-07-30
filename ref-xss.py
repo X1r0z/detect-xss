3e3466c9e2aea11af74e080783b7a70605c6e310
X1r0z <admin@exp10it.cn>
2018-07-30 13:59:18 +0800

    Fix url split problem

--- ref-xss.py
+++ ref-xss.py
@@ -4,31 +4,36 @@ import requests
 import random
 import string
 import re
+import os
+import sys
 
 url = 'http://127.0.0.1/xss-example/';
 
 def get_dynamic_links_url(murl):
     '''
     Read dynamic links from url, only GET
-    
+
     result:
         {'http://127.0.0.1/xss.php': ['id', 'form', 'callback', 'protect1', 'protect2']}
     '''
-    url_links = dict() 
+    url_links = dict()
     response = requests.get(murl)
     html = bs(response.text,'html.parser')
     a = html.find_all(name='a')
     for el in a:
         parse = urlsplit(el['href'])
         query = parse[3]
-            ''''
-            1. adasd.php
-            2. /a/safasf/asd.php
-            3. http://www.adasd.com/adfsad.php
-            4. //www.asdsad.com/adsasd.php
-
-            '''
-        if query
+        host = murl.split('//',1)[1].split('/',1)[0]
+        dirname = os.path.dirname(murl.split('//',1)[1].split('/',1)[1])
+        if el['href'].startswith('http'):
+            url = el['href']
+        elif el['href'].startswith('//'):
+            url = murl.split('//',1)[0] + el['href']
+        elif el['href'].startswith('/'):
+            url = murl.split('//',1)[0] + '//' + host + el['href']
+        else:
+            url = murl.split('//',1)[0] + '//' + host + '/' + dirname + '/' + el['href']
+        if query:
             print(el['href'],'is dynamic')
             if not url in url_links:
                 url_links[url] = list()
@@ -91,7 +96,14 @@ def get_output_position(ulinks):
             for pos in position:
                 if pos == rnd:
                     print('output on the page')
-
+                elif re.findall(regex_tags,pos):
+                    print('output on the tags')
+                elif re.findall(regex_attrs,pos):
+                    print('output one the attrs')
+                elif re.findall(regex_js,pos):
+                    print('output on the js')
+                else:
+                    print('no output')
 
 ulinks = get_dynamic_links_url(url)
 flinks = get_dynamic_links_form(url)
